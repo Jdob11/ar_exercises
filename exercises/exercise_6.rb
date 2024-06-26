7bdf3628eb3f650daa799ae4f22580b012074a7a
Jdob11 <jbdobson11@gmail.com>
2024-06-26 14:38:00 -0700

    database: Create associate between stores and employees databases and add employees to stores 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,16 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Jim", last_name: "Halpert", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Dwight", last_name: "Schrute", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Andy", last_name: "Bernard", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Karen", last_name: "Filippelli", hourly_rate: 100)
+
+@employees = Employee.all
+
+@employees.each do |employee|
+  puts "#{employee.last_name}, #{employee.first_name}"
+end
